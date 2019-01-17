c15b6ca1e86c98aa18eb0717bfecf1e76a6347d0
Bertil Chapuis <bchapuis@gmail.com>
2019-01-17 23:53:21 +0100

    test wrapped exceptions

--- gazetteer-osm/src/test/java/io/gazetteer/osm/osmpbf/FileBlockConstants.java
+++ gazetteer-osm/src/test/java/io/gazetteer/osm/osmpbf/FileBlockConstants.java
@@ -1,9 +1,13 @@
 package io.gazetteer.osm.osmpbf;
 
+import com.google.protobuf.ByteString;
+
 import java.io.File;
 
 public class FileBlockConstants {
 
     public static final File TEN_BLOCKS = new File(FileBlockConstants.class.getClassLoader().getResource("ten_blocks.osm").getFile());
+
+    public static final FileBlock INVALID_BLOCK = new FileBlock("", ByteString.copyFromUtf8(""), ByteString.copyFromUtf8(""));
 
 }
--- gazetteer-osm/src/test/java/io/gazetteer/osm/osmpbf/FileBlocksTest.java
+++ gazetteer-osm/src/test/java/io/gazetteer/osm/osmpbf/FileBlocksTest.java
@@ -1,11 +1,14 @@
 package io.gazetteer.osm.osmpbf;
 
+import com.google.protobuf.ByteString;
+import io.gazetteer.osm.util.WrappedException;
 import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
+import static io.gazetteer.osm.osmpbf.FileBlockConstants.INVALID_BLOCK;
 import static io.gazetteer.osm.osmpbf.FileBlockConstants.TEN_BLOCKS;
 import static org.junit.Assert.assertTrue;
 
@@ -43,6 +46,11 @@ public class FileBlocksTest {
                 .count() == 1);
     }
 
+    @Test(expected = WrappedException.class)
+    public void toHeaderaBlockException() {
+        FileBlocks.toHeaderBlock(INVALID_BLOCK);
+    }
+
     @Test
     public void toDataBlock() throws FileNotFoundException {
         assertTrue(FileBlocks
@@ -50,6 +58,11 @@ public class FileBlocksTest {
                 .filter(FileBlocks::isDataBlock)
                 .map(FileBlocks::toDataBlock)
                 .collect(Collectors.toList()).size() == 9);
+    }
+
+    @Test(expected = WrappedException.class)
+    public void toDataBlockException() {
+        FileBlocks.toDataBlock(INVALID_BLOCK);
     }
 
 }
