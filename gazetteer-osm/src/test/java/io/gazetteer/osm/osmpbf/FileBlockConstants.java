6c15ca91f9f9b09dae05ace50750a4405f465483
Bertil Chapuis <bchapuis@gmail.com>
2019-01-19 16:53:42 +0100

    improve test coverage

--- gazetteer-osm/src/test/java/io/gazetteer/osm/osmpbf/FileBlockConstants.java
+++ gazetteer-osm/src/test/java/io/gazetteer/osm/osmpbf/FileBlockConstants.java
@@ -8,6 +8,12 @@ public class FileBlockConstants {
 
     public static final File BLOCKS = new File(FileBlockConstants.class.getClassLoader().getResource("blocks.osm").getFile());
 
+    public static final File DENSE_BLOCK = new File(FileBlockConstants.class.getClassLoader().getResource("dense.osm").getFile());
+
+    public static final File WAYS_BLOCK = new File(FileBlockConstants.class.getClassLoader().getResource("ways.osm").getFile());
+
+    public static final File RELATIONS_BLOCK = new File(FileBlockConstants.class.getClassLoader().getResource("relations.osm").getFile());
+
     public static final FileBlock INVALID_BLOCK = new FileBlock("", ByteString.copyFromUtf8(""), ByteString.copyFromUtf8(""));
 
 }
--- gazetteer-osm/src/test/java/io/gazetteer/osm/osmpbf/PrimitiveBlockReaderTest.java
+++ gazetteer-osm/src/test/java/io/gazetteer/osm/osmpbf/PrimitiveBlockReaderTest.java
@@ -1,11 +1,15 @@
 package io.gazetteer.osm.osmpbf;
 
+import io.gazetteer.osm.domain.Node;
+import io.gazetteer.osm.domain.Relation;
+import io.gazetteer.osm.domain.Way;
 import org.junit.Test;
 import org.openstreetmap.osmosis.osmbinary.Osmformat;
 
 import java.io.IOException;
+import java.util.List;
 
-import static io.gazetteer.osm.osmpbf.FileBlockConstants.BLOCKS;
+import static io.gazetteer.osm.osmpbf.FileBlockConstants.*;
 import static org.junit.Assert.*;
 
 public class PrimitiveBlockReaderTest {
@@ -17,9 +21,40 @@ public class PrimitiveBlockReaderTest {
         PrimitiveBlockReader primitiveBlockReader = new PrimitiveBlockReader(Osmformat.PrimitiveBlock.parseFrom(headerBlock.getData()));
         PrimitiveBlock primitiveBlock = primitiveBlockReader.read();
         assertNotNull(primitiveBlock);
-        assertTrue(primitiveBlock.getNodes().isEmpty());
-        assertTrue(primitiveBlock.getWays().isEmpty());
-        assertTrue(primitiveBlock.getRelations().isEmpty());
+        for (int i = 0; i < 9; i++) {
+            primitiveBlock = primitiveBlockReader.read();
+            assertNotNull(primitiveBlock);
+        }
+    }
+
+    @Test
+    public void readDenseNodes() throws IOException {
+        FileBlockReader reader = FileBlockUtil.reader(DENSE_BLOCK);
+        FileBlock block = reader.read();
+        PrimitiveBlockReader primitiveBlockReader = new PrimitiveBlockReader(Osmformat.PrimitiveBlock.parseFrom(block.getData()));
+        List<Node> nodes = primitiveBlockReader.readDenseNodes();
+        assertNotNull(nodes);
+        assertFalse(nodes.isEmpty());
+    }
+
+    @Test
+    public void readWays() throws IOException {
+        FileBlockReader reader = FileBlockUtil.reader(WAYS_BLOCK);
+        FileBlock block = reader.read();
+        PrimitiveBlockReader primitiveBlockReader = new PrimitiveBlockReader(Osmformat.PrimitiveBlock.parseFrom(block.getData()));
+        List<Way> ways = primitiveBlockReader.readWays();
+        assertNotNull(ways);
+        assertFalse(ways.isEmpty());
+    }
+
+    @Test
+    public void readRelations() throws IOException {
+        FileBlockReader reader = FileBlockUtil.reader(RELATIONS_BLOCK);
+        FileBlock block = reader.read();
+        PrimitiveBlockReader primitiveBlockReader = new PrimitiveBlockReader(Osmformat.PrimitiveBlock.parseFrom(block.getData()));
+        List<Relation> relations = primitiveBlockReader.readRelations();
+        assertNotNull(relations);
+        assertFalse(relations.isEmpty());
     }
 
 }
