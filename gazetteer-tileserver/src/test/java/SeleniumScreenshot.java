a75526078b065087395c089d9bd9be5ec0c19426
Bertil Chapuis <bchapuis@gmail.com>
2019-02-12 22:41:05 +0100

    improved the status of the map

--- gazetteer-tileserver/src/test/java/SeleniumScreenshot.java
+++ gazetteer-tileserver/src/test/java/SeleniumScreenshot.java
@@ -1,26 +1,22 @@
-import io.gazetteer.tileserver.TileServer;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
 
 public class SeleniumScreenshot {
-        public static void main(String[] args) throws IOException {
-            TileServer.main(new String[] {"config/config.yaml"});
-            System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        public static void main(String[] args) throws IOException, InterruptedException {
             WebDriver driver = new ChromeDriver();
-            driver.manage().window().maximize();
-            driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+            driver.manage().window().setSize(new Dimension(1600, 1200));
             driver.get("http://localhost:8081/");
+            Thread.sleep(1000);
             File source_file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-            Files.copy(source_file.toPath(), Paths.get(String.format("%s.png", System.currentTimeMillis())));
+            Files.copy(source_file.toPath(), Paths.get("screenshots").resolve(String.format("%s.png", System.currentTimeMillis())));
             driver.quit();
         }
 }
