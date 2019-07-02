3eac10d3ef3ea9e4ed4919896e652b0776cf3e71
Bertil Chapuis <bchapuis@gmail.com>
2019-07-02 22:28:05 +0200

    Remove unused methods and classes

--- gazetteer-osm/src/main/java/io/gazetteer/osm/model/DataStoreException.java
+++
@@ -1,8 +0,0 @@
-package io.gazetteer.osm.model;
-
-public class DataStoreException extends Exception {
-
-  public DataStoreException(Exception e) {
-    super(e);
-  }
-}
--- gazetteer-osm/src/main/java/io/gazetteer/osm/util/GeometryUtil.java
+++ gazetteer-osm/src/main/java/io/gazetteer/osm/util/GeometryUtil.java
@@ -33,39 +33,6 @@ public class GeometryUtil {
     return new GeometryFactory(new PrecisionModel(), 3857).createPoint(coordinate);
   }
 
-  public static Geometry asGeometry(Way way, DataStore<Long, Node> nodeStore)
-      throws DataStoreException {
-    GeometryFactory geometryFactory = new GeometryFactory();
-    List<Long> ids = way.getNodes();
-    List<Node> nodes = nodeStore.getAll(ids);
-    List<Coordinate> list = new ArrayList<>();
-    for (Node node : nodes) {
-      Coordinate coordinate = coordinate(node.getLon(), node.getLat());
-      list.add(coordinate);
-    }
-    Coordinate[] coordinates = list.toArray(new Coordinate[0]);
-    if (ids.get(0).equals(ids.get(ids.size() - 1)) && ids.size() > 3) {
-      return geometryFactory.createPolygon(coordinates);
-    } else if (ids.size() > 1) {
-      return geometryFactory.createLineString(coordinates);
-    } else {
-      throw new IllegalArgumentException();
-    }
-  }
-
-  public static Geometry asGeometryWithWrappedException(
-      Way way, DataStore<Long, Node> nodeEntityStore) {
-    try {
-      return GeometryUtil.asGeometry(way, nodeEntityStore);
-    } catch (DataStoreException e) {
-      throw new WrappedException(e);
-    }
-  }
-
-  public static Geometry asGeometry(Relation relation) {
-    throw new UnsupportedOperationException();
-  }
-
   public static Geometry asGeometry(byte[] wkb) {
     try {
       WKBReader reader = new WKBReader(new GeometryFactory());
