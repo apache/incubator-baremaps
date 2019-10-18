df0b47c5b6eea64316d12470294527c3b344525c
Bertil Chapuis <bchapuis@gmail.com>
2019-10-18 15:41:12 +0200

    Disable benchmarking

--- gazetteer-benchmarks/src/main/java/io/gazetteer/PostgisConsumerBenchmark.java
+++ gazetteer-benchmarks/src/main/java/io/gazetteer/PostgisConsumerBenchmark.java
@@ -1,42 +1,14 @@
 package io.gazetteer;
 
-import io.gazetteer.osm.osmpbf.PrimitiveBlock;
-import org.openjdk.jmh.annotations.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 
 @State(Scope.Benchmark)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @Fork(1)
 public class PostgisConsumerBenchmark {
 
-  public Stream<PrimitiveBlock> stream;
-
-  public Consumer consumer;
-  /*
-  @Setup(Level.Invocation)
-  public void prepare() throws SQLException, IOException {
-    try (Connection connection = DriverManager.getConnection(POSTGRES_URL)) {
-      URL url = Resources.getResource("osm_create_tables.sql");
-      String sql = Resources.toString(url, Charsets.UTF_8);
-      connection.createStatement().execute(sql);
-    }
-    PoolingDataSource pool = DatabaseUtils.poolingDataSource(POSTGRES_URL);
-    Cache<Coordinate> coordinateMap = new Cache<Coordinate>(new CoordinateMapper());
-    consumer = new BlockConsumer(pool, coordinateMap);
-    InputStream input = Files.newInputStream(Paths.get(PBF_FILE));
-    stream = PBFUtil.toPrimitiveBlock(PBFUtil.stream(input));
-  }
-
-  @Benchmark
-  @BenchmarkMode(Mode.AverageTime)
-  @Warmup(iterations = 2)
-  @Measurement(iterations = 5)
-  public void processStream() throws ExecutionException, InterruptedException {
-    ForkJoinPool executor = new ForkJoinPool(1);
-    executor.submit(() -> stream.forEach(consumer)).get();
-  }
-  */
 }
