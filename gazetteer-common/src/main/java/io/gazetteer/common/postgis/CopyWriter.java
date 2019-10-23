7b0fc9dcfa4c26f43f0f9672eefde54d0483ad14
Bertil Chapuis <bchapuis@gmail.com>
2019-10-23 14:06:25 +0200

    Add documentation to CopyWriter

--- gazetteer-common/src/main/java/io/gazetteer/common/postgis/CopyWriter.java
+++ gazetteer-common/src/main/java/io/gazetteer/common/postgis/CopyWriter.java
@@ -17,6 +17,9 @@ import java.util.Map;
 import org.locationtech.jts.geom.Geometry;
 import org.postgresql.copy.PGCopyOutputStream;
 
+/**
+ * A helper for writing in a {@code PGCopyOutputStream}.
+ */
 public class CopyWriter implements AutoCloseable {
 
   private static final Charset UTF8 = Charset.forName("UTF-8");
@@ -31,10 +34,18 @@ public class CopyWriter implements AutoCloseable {
 
   private final DataOutputStream data;
 
+  /**
+   * Constructs a new writer with the specified {@code PGCopyOutputStream}.
+   * @param data
+   */
   public CopyWriter(PGCopyOutputStream data) {
     this.data = new DataOutputStream(new BufferedOutputStream(data, 65536));
   }
 
+  /**
+   * Writes the header of the query.
+   * @throws IOException
+   */
   public void writeHeader() throws IOException {
     // 11 bytes required header
     data.writeBytes("PGCOPY\n\377\r\n\0");
@@ -44,102 +55,225 @@ public class CopyWriter implements AutoCloseable {
     data.writeInt(0);
   }
 
+  /**
+   * Writes the number of columns affected by the query.
+   * @param columns
+   * @throws IOException
+   */
   public void startRow(int columns) throws IOException {
     data.writeShort(columns);
   }
 
+  /**
+   * Writes a null value.
+   * @throws IOException
+   */
   public void writeNull() throws IOException {
     data.writeInt(-1);
   }
 
+  /**
+   * Writes a string value.
+   * @param value
+   * @throws IOException
+   */
   public void writeString(String value) throws IOException {
     nullableWriter(CopyWriter::stringWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of string values.
+   * @param value
+   * @throws IOException
+   */
   public void writeStringList(List<String> value) throws IOException {
     nullableWriter(collectionWriter(ObjectIdentifier.Text, CopyWriter::stringWriter)).write(data, value);
   }
 
+  /**
+   * Writes a boolean value.
+   * @param value
+   * @throws IOException
+   */
   public void writeBoolean(Boolean value) throws IOException {
     nullableWriter(CopyWriter::booleanWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of boolean values.
+   * @param value
+   * @throws IOException
+   */
   public void writeBooleanList(List<Boolean> value) throws IOException {
     nullableWriter(collectionWriter(ObjectIdentifier.Boolean, CopyWriter::booleanWriter)).write(data, value);
   }
 
+  /**
+   * Writes a byte value.
+   * @param value
+   * @throws IOException
+   */
   public void writeByte(Byte value) throws IOException {
     nullableWriter(CopyWriter::byteWriter).write(data, value);
   }
 
+  /**
+   * Writes a byte array value.
+   * @param value
+   * @throws IOException
+   */
   public void writeByteArray(byte[] value) throws IOException {
     nullableWriter(CopyWriter::byteArrayWriter).write(data, value);
   }
 
+  /**
+   * Writes a short value.
+   * @param value
+   * @throws IOException
+   */
   public void writeShort(Short value) throws IOException {
     nullableWriter(CopyWriter::shortWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of short values.
+   * @param value
+   * @throws IOException
+   */
   public void writeShortList(List<Short> value) throws IOException {
     nullableWriter(collectionWriter(ObjectIdentifier.Int4, CopyWriter::shortWriter)).write(data, value);
   }
 
+  /**
+   * Writes an integer value.
+   * @param value
+   * @throws IOException
+   */
   public void writeInteger(Integer value) throws IOException {
     nullableWriter(CopyWriter::integerWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of integer values.
+   * @param value
+   * @throws IOException
+   */
   public void writeIntegerList(List<Integer> value) throws IOException {
     nullableWriter(collectionWriter(ObjectIdentifier.Int4, CopyWriter::integerWriter)).write(data, value);
   }
 
+  /**
+   * Writes a long value.
+   * @param value
+   * @throws IOException
+   */
   public void writeLong(Long value) throws IOException {
     nullableWriter(CopyWriter::longWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of long values.
+   * @param value
+   * @throws IOException
+   */
   public void writeLongList(List<Long> value) throws IOException {
     nullableWriter(collectionWriter(ObjectIdentifier.Int8, CopyWriter::longWriter)).write(data, value);
   }
 
+  /**
+   * Writes a float value.
+   * @param value
+   * @throws IOException
+   */
   public void writeFloat(Float value) throws IOException {
     nullableWriter(CopyWriter::floatWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of float values.
+   * @param value
+   * @throws IOException
+   */
   public void writeFloatList(List<Float> value) throws IOException {
     nullableWriter(collectionWriter(ObjectIdentifier.Int8, CopyWriter::floatWriter)).write(data, value);
   }
 
+  /**
+   * Writes a double value.
+   * @param value
+   * @throws IOException
+   */
   public void writeDouble(Double value) throws IOException {
     nullableWriter(CopyWriter::doubleWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of double values.
+   * @param value
+   * @throws IOException
+   */
   public void writeDoubleArray(List<Double> value) throws IOException {
     nullableWriter(collectionWriter(ObjectIdentifier.Int8, CopyWriter::doubleWriter)).write(data, value);
   }
 
+  /**
+   * Writes a date value.
+   * @param value
+   * @throws IOException
+   */
   public void writeLocalDate(LocalDate value) throws IOException {
     nullableWriter(CopyWriter::localDateWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of date values.
+   * @param value
+   * @throws IOException
+   */
   public void writeLocalDateTime(LocalDateTime value) throws IOException {
     nullableWriter(CopyWriter::localDateTimeWriter).write(data, value);
   }
 
+  /**
+   * Writes an inet adress value.
+   * @param value
+   * @throws IOException
+   */
   public void writeInet4Adress(Inet4Address value) throws IOException {
     nullableWriter(CopyWriter::inet4AdressWriter).write(data, value);
   }
 
+  /**
+   * Writes a list of inet adress values.
+   * @param value
+   * @throws IOException
+   */
   public void writeInet6Adress(Inet6Address value) throws IOException {
     nullableWriter(CopyWriter::inet6AdressWriter).write(data, value);
   }
 
+  /**
+   * Writes a map value.
+   * @param value
+   * @throws IOException
+   */
   public void writeHstore(Map<String, String> value) throws IOException {
     nullableWriter(CopyWriter::hstoreWriter).write(data, value);
   }
 
+  /**
+   * Writes a geometry value.
+   * @param value
+   * @throws IOException
+   */
   public void writeGeometry(Geometry value) throws IOException {
     nullableWriter(CopyWriter::byteArrayWriter).write(data, GeometryUtils.toWKB(value));
   }
 
+  /**
+   * Close the writer.
+   * @throws Exception
+   */
   @Override
   public void close() throws Exception {
     data.writeShort(-1);
