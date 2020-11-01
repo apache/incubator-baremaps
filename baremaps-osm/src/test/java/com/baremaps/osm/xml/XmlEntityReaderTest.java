25e823b4d06fce323f981893cff88705104c4e79
Bertil Chapuis <bchapuis@gmail.com>
2020-11-01 22:10:50 +0100

    Add XmlEntityReader test

--- baremaps-osm/src/test/java/com/baremaps/osm/xml/XmlEntityReaderTest.java
+++ baremaps-osm/src/test/java/com/baremaps/osm/xml/XmlEntityReaderTest.java
@@ -1,4 +1,4 @@
-package com.baremaps.osm.pbf;
+package com.baremaps.osm.xml;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,27 +6,29 @@ import com.baremaps.osm.DataFiles;
 import com.baremaps.osm.domain.Node;
 import com.baremaps.osm.domain.Relation;
 import com.baremaps.osm.domain.Way;
+import com.baremaps.osm.pbf.PbfEntityReader;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
-class PbfEntityReaderTest {
+class XmlEntityReaderTest {
 
   @Test
   void entities() throws IOException {
-    assertEquals(72002, new PbfEntityReader(DataFiles.dataOsmPbf())
+    assertEquals(12, new XmlEntityReader(DataFiles.dataOsmXml())
         .entities()
         .count());
-    assertEquals(8000, new PbfEntityReader(DataFiles.denseNodesOsmPbf())
+    assertEquals(6, new XmlEntityReader(DataFiles.dataOsmXml())
         .entities()
         .filter(e -> e instanceof Node)
         .count());
-    assertEquals(8000, new PbfEntityReader(DataFiles.waysOsmPbf())
+    assertEquals(3, new XmlEntityReader(DataFiles.dataOsmXml())
         .entities()
         .filter(e -> e instanceof Way)
         .count());
-    assertEquals(8000, new PbfEntityReader(DataFiles.relationsOsmPbf())
+    assertEquals(1, new XmlEntityReader(DataFiles.dataOsmXml())
         .entities()
         .filter(e -> e instanceof Relation)
         .count());
   }
+
 }
