3c21562c4f7bc9cd79da384586dd845e32f02650
Bertil Chapuis <bchapuis@gmail.com>
2021-02-09 21:32:06 +0100

    Fix tests

--- baremaps-config/src/main/java/com/baremaps/config/mapping/MappingLoader.java
+++ baremaps-config/src/main/java/com/baremaps/config/mapping/MappingLoader.java
@@ -3,7 +3,7 @@ package com.baremaps.config.mapping;
 import com.baremaps.blob.BlobStore;
 import com.baremaps.config.ConfigLoader;
 
-public class MappingLoader extends ConfigLoader {
+public class MappingLoader extends ConfigLoader<Mapping> {
 
   public MappingLoader(BlobStore blobStore) {
     super(blobStore, Mapping.class);
--- baremaps-config/src/test/java/com/baremaps/config/MappingLoaderTest.java
+++ baremaps-config/src/test/java/com/baremaps/config/MappingLoaderTest.java
@@ -1,10 +1,10 @@
 package com.baremaps.config;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.baremaps.blob.ResourceBlobStore;
-import com.baremaps.config.style.Style;
-import com.baremaps.config.style.StyleLoader;
+import com.baremaps.config.mapping.Mapping;
+import com.baremaps.config.mapping.MappingLoader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -14,14 +14,10 @@ class MappingLoaderTest {
 
   @Test
   public void load() throws URISyntaxException, IOException {
-    Style style = new StyleLoader(new ResourceBlobStore()).load(new URI("res://./mapping.yaml"));
-    assertEquals(style.getId(), "style");
-    assertEquals(style.getSheets().size(), 2);
-    assertEquals(style.getSheets().get(0).getId(), "sheet");
-    assertEquals(style.getSheets().get(1).getId(), "sheet");
-    assertEquals(style.getSheets().get(1).getLayers().get(0).getId(), "layer");
-    assertEquals(style.getSheets().get(1).getLayers().get(0).getSourceLayer(), "source-layer");
+    Mapping mapping = new MappingLoader(new ResourceBlobStore()).load(new URI("res://./mapping.yaml"));
+    assertEquals(mapping.getAllowedTags().size(), 2);
+    assertEquals(mapping.getAllowedTags().get(0), "tag1");
+    assertEquals(mapping.getAllowedTags().get(1), "tag2");
   }
-
 
 }
