6fc188be7c2a3494631424dd8aeb6d3a64aa5701
Bertil Chapuis <bchapuis@gmail.com>
2021-02-27 12:34:04 +0100

    Add javadoc

--- baremaps-config/src/main/java/com/baremaps/config/Variables.java
+++ baremaps-config/src/main/java/com/baremaps/config/Variables.java
@@ -3,6 +3,9 @@ package com.baremaps.config;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * A utility class to interpolate variables in strings.
+ */
 public class Variables {
 
   public static String interpolate(Map<String, String> variables, String string) {
