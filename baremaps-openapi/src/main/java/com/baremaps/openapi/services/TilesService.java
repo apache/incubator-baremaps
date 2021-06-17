5521a7f10af762358ed061448564c27616cbf37b
Balthasar Teuscher <balthasar.teuscher@camptocamp.com>
2021-06-17 12:38:33 +0200

    Add /tiles path

--- baremaps-openapi/src/main/java/com/baremaps/openapi/services/TilesService.java
+++ baremaps-openapi/src/main/java/com/baremaps/openapi/services/TilesService.java
@@ -1,9 +1,17 @@
 package com.baremaps.openapi.services;
 
 import com.baremaps.api.TilesApi;
+
+import com.baremaps.model.TileSetsDescription;
 import java.util.List;
 
 public class TilesService implements TilesApi {
+
+
+  @Override
+  public TileSetsDescription describeTilesCollections() {
+    return null;
+  }
 
   @Override
   public String getTileSQL(String tileMatrixSetId, String tileMatrix, Integer tileRow, Integer tileCol,
