248aef9e37b73ad8a174e855aa3bb704f4041589
Balthasar Teuscher <balthasar.teuscher@gmail.com>
2021-07-07 10:50:24 +0200

    Add tilesets endpoints

---
+++ baremaps-openapi/src/main/java/com/baremaps/openapi/services/TilesetsService.java
@@ -0,0 +1,31 @@
+package com.baremaps.openapi.services;
+
+import com.baremaps.api.TilesetsApi;
+
+public class TilesetsService implements TilesetsApi {
+
+  @Override
+  public void addTileset(Object body) {
+
+  }
+
+  @Override
+  public void deleteTileset(String tilesetId) {
+
+  }
+
+  @Override
+  public Object getTileset(String tilesetId) {
+    return null;
+  }
+
+  @Override
+  public Object getTilesets() {
+    return null;
+  }
+
+  @Override
+  public void updateTileset(String tilesetId, Object body) {
+
+  }
+}
