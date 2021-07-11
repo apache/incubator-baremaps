63bfaea1600e48887050819b92264c52f0295b37
Bertil Chapuis <bchapuis@gmail.com>
2021-07-11 20:50:47 +0200

    Configure jersey test framework

--- baremaps-openapi/src/main/java/com/baremaps/openapi/BaremapsServer.java
+++ baremaps-openapi/src/main/java/com/baremaps/openapi/BaremapsServer.java
@@ -1,5 +1,11 @@
 package com.baremaps.openapi;
 
+import com.baremaps.openapi.services.CollectionsService;
+import com.baremaps.openapi.services.ConformanceService;
+import com.baremaps.openapi.services.RootService;
+import com.baremaps.openapi.services.StylesService;
+import com.baremaps.openapi.services.TilesService;
+import com.baremaps.openapi.services.TilesetsService;
 import com.baremaps.postgres.jdbc.PostgresUtils;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,6 +14,7 @@ import io.servicetalk.http.router.jersey.HttpJerseyRouterBuilder;
 import io.servicetalk.transport.api.ServerContext;
 import javax.sql.DataSource;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.jackson2.Jackson2Config;
 import org.jdbi.v3.jackson2.Jackson2Plugin;
@@ -41,15 +48,25 @@ public class BaremapsServer {
     mapper.setSerializationInclusion(Include.NON_NULL);
     jdbi.getConfig(Jackson2Config.class).setMapper(mapper);
 
+    // Initialize the application
+    ResourceConfig application = new ResourceConfig()
+        .registerClasses(RootService.class,
+            ConformanceService.class,
+            CollectionsService.class,
+            StylesService.class,
+            TilesetsService.class,
+            TilesService.class)
+        .register(new AbstractBinder() {
+          @Override
+          protected void configure() {
+            bind(jdbi).to(Jdbi.class);
+          }
+        });
+
     // Create configurable starter for HTTP server.
     ServerContext serverContext = HttpServers.forPort(8080)
         .listenStreamingAndAwait(new HttpJerseyRouterBuilder()
-            .buildStreaming(new BaremapsApplication(new AbstractBinder() {
-              @Override
-              protected void configure() {
-                bind(jdbi).to(Jdbi.class);
-              }
-            })));
+            .buildStreaming(application));
 
     LOGGER.info("Listening on {}", serverContext.listenAddress());
 
