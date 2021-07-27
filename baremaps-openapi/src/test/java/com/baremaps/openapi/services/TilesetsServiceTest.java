8d67bcea0daac4e347d4dfa74606a2ccf564273b
Bertil Chapuis <bchapuis@gmail.com>
2021-07-27 15:28:32 +0200

    Add sample integration test

---
+++ baremaps-openapi/src/test/java/com/baremaps/openapi/services/TilesetsServiceTest.java
@@ -0,0 +1,88 @@
+package com.baremaps.openapi.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.baremaps.model.TileSet;
+import com.baremaps.postgres.jdbi.PostgisPlugin;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.TestProperties;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.jackson2.Jackson2Plugin;
+import org.junit.Test;
+
+public class TilesetsServiceTest extends JerseyTest {
+
+  Jdbi jdbi;
+
+  @Override
+  protected ResourceConfig configure() {
+    enable(TestProperties.LOG_TRAFFIC);
+    enable(TestProperties.DUMP_ENTITY);
+
+    // Create a connection to an throwaway postgis database
+    Connection connection;
+    try {
+      connection = DriverManager.getConnection("jdbc:tc:postgis:13-3.1:///test");
+    } catch (SQLException throwables) {
+      throw new RuntimeException("Unable to connect to the database");
+    }
+
+    // Initialize the database
+    jdbi = Jdbi.create(connection)
+        .installPlugin(new PostgisPlugin())
+        .installPlugin(new Jackson2Plugin());
+    jdbi.useHandle(handle -> {
+      handle.execute("create table tilesets (id varchar primary key, tileset jsonb)");
+    });
+
+    // Configure the service
+    return new ResourceConfig()
+        .register(TilesetsService.class)
+        .register(new AbstractBinder() {
+          @Override
+          protected void configure() {
+            bind(jdbi).to(Jdbi.class);
+          }
+        });
+  }
+
+  @Test
+  public void test() {
+    // List the tilesets
+    List<String> ids = target().path("/tilesets").request().get(new GenericType<>() {});
+    assertEquals(0, ids.size());
+
+    // Create a new tileset with the service
+    TileSet tileSet = new TileSet();
+    tileSet.setName("test");
+    target().path("/tilesets")
+        .request(MediaType.APPLICATION_JSON)
+        .post(Entity.json(tileSet));
+
+    // List the tilesets
+    ids = target().path("/tilesets").request().get(new GenericType<>() {});
+    assertEquals(1, ids.size());
+
+    // Get the tileset
+    String id = ids.get(0);
+    tileSet = target().path("/tilesets/" + id).request().get(TileSet.class);
+    assertEquals("test", tileSet.getName());
+
+    // Delete the tileset
+    target().path("/tilesets/" + ids.get(0)).request().delete();
+
+    // List the tilesets
+    ids = target().path("/tilesets").request().get(new GenericType<>() {});
+    assertEquals(0, ids.size());
+  }
+
+}
