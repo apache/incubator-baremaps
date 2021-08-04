1a5610f019a67b08650c503cc1786a3f6166fe53
Balthasar Teuscher <balthasar.teuscher@gmail.com>
2021-08-04 16:19:04 +0200

    Fix get tile & cleanup

--- baremaps-openapi/src/test/java/com/baremaps/openapi/TilsetQueryParserTest.java
+++ baremaps-openapi/src/test/java/com/baremaps/openapi/TilsetQueryParserTest.java
@@ -7,6 +7,7 @@ import com.baremaps.model.Query;
 import com.baremaps.model.TileSet;
 import com.baremaps.tile.Tile;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class TilsetQueryParserTest {
@@ -20,16 +21,17 @@ public class TilsetQueryParserTest {
 
     Layer a = new Layer();
     a.setId("a");
-    a.setQueries(Arrays.asList(q));
+    a.setQueries(List.of(q));
 
     Layer b = new Layer();
     b.setId("b");
-    b.setQueries(Arrays.asList(q));
+    b.setQueries(List.of(q));
 
     TileSet tileset = new TileSet();
     tileset.setLayers(Arrays.asList(a, b));
 
-    String query = TilesetQueryParser.parse(tileset, new Tile(0, 0, 10));
+    TilesetQueryParser parser = new TilesetQueryParser();
+    String query = parser.parse(tileset, new Tile(0, 0, 10));
     assertEquals("with ha14cb45b as (select id, (tags || hstore('geometry', lower(replace(st_geometrytype(geom), 'ST_', '')))) as tags, st_asmvtgeom(geom, st_tileenvelope(10, 0, 0), 4096, 256, true) as geom from (select id as id, tags as tags, geom as geom from table) as source where  st_intersects(geom, st_tileenvelope(10, 0, 0))) select st_asmvt(target, 'a', 4096, 'geom', 'id') from (select id, hstore_to_jsonb_loose(tags) as tags, geom from ha14cb45b ) as target union all select st_asmvt(target, 'b', 4096, 'geom', 'id') from (select id, hstore_to_jsonb_loose(tags) as tags, geom from ha14cb45b ) as target", query);
   }
 
@@ -42,7 +44,7 @@ public class TilsetQueryParserTest {
 
     Layer a = new Layer();
     a.setId("a");
-    a.setQueries(Arrays.asList(q1));
+    a.setQueries(List.of(q1));
 
     Query q2 = new Query();
     q2.setMinzoom(0);
@@ -51,12 +53,13 @@ public class TilsetQueryParserTest {
 
     Layer b = new Layer();
     b.setId("b");
-    b.setQueries(Arrays.asList(q2));
+    b.setQueries(List.of(q2));
 
     TileSet tileset = new TileSet();
     tileset.setLayers(Arrays.asList(a, b));
 
-    String query = TilesetQueryParser.parse(tileset, new Tile(0, 0, 10));
+    TilesetQueryParser parser = new TilesetQueryParser();
+    String query = parser.parse(tileset, new Tile(0, 0, 10));
     assertEquals("with ha14cb45b as (select id, (tags || hstore('geometry', lower(replace(st_geometrytype(geom), 'ST_', '')))) as tags, st_asmvtgeom(geom, st_tileenvelope(10, 0, 0), 4096, 256, true) as geom from (select id as id, tags as tags, geom as geom from table) as source where ((condition = 1) OR (condition = 2)) and st_intersects(geom, st_tileenvelope(10, 0, 0))) select st_asmvt(target, 'a', 4096, 'geom', 'id') from (select id, hstore_to_jsonb_loose(tags) as tags, geom from ha14cb45b where condition = 1) as target union all select st_asmvt(target, 'b', 4096, 'geom', 'id') from (select id, hstore_to_jsonb_loose(tags) as tags, geom from ha14cb45b where condition = 2) as target", query);
   }
 
