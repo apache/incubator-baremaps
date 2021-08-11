bde5ba6f7b1f893c5b883c95d23fadf91c758c77
Rémi Desgrange <remi.desgrange@camptocamp.com>
2021-08-11 12:17:44 +0200

    Add resteasy licence mention + fix tests

--- baremaps-server/src/main/java/com/baremaps/server/CorsFilter.java
+++ baremaps-server/src/main/java/com/baremaps/server/CorsFilter.java
@@ -1,6 +1,12 @@
+/*
+ * Code inspired by resteasy CorsFilter: https://github.com/resteasy/Resteasy/blob/22984e96a2f40dc556ba7b1613422febd94f86d2/resteasy-core/src/main/java/org/jboss/resteasy/plugins/interceptors/CorsFilter.java
+ *
+ Copyright (c) Resteasy Maintainers. All rights reserved.
+ Licensed under the ASL license.
+ */
+
 package com.baremaps.server;
 
-import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -10,52 +16,46 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
-
-  public static class CorsHeaders
-  {
-    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
-    public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
-    public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
-    public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
-    public static final String ORIGIN = "Origin";
-    public static final String VARY = "Vary";
-  }
+  
+    private final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    private final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    private final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    private final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    private final String ORIGIN = "Origin";
+    private final String VARY = "Vary";
 
   @Override
-  public void filter(ContainerRequestContext requestContext) throws IOException
-  {
-    String origin = requestContext.getHeaderString(CorsHeaders.ORIGIN);
+  public void filter(ContainerRequestContext requestContext) {
+    String origin = requestContext.getHeaderString(ORIGIN);
     if (origin == null) {
       return;
     }
-    if (requestContext.getMethod().equalsIgnoreCase("OPTIONS"))
-    {
+    if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
       preflight(origin, requestContext);
     }
   }
 
   @Override
-  public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException
-  {
-    String origin = requestContext.getHeaderString(CorsHeaders.ORIGIN);
-    if (origin == null || requestContext.getMethod().equalsIgnoreCase("OPTIONS") || requestContext.getProperty("cors.failure") != null)
-    {
+  public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+    String origin = requestContext.getHeaderString(ORIGIN);
+    if (origin == null || requestContext.getMethod().equalsIgnoreCase("OPTIONS") || requestContext.getProperty("cors.failure") != null) {
       return;
     }
-    responseContext.getHeaders().putSingle(CorsHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
-    responseContext.getHeaders().putSingle(CorsHeaders.VARY, CorsHeaders.ORIGIN);
+    responseContext.getHeaders().putSingle(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+    responseContext.getHeaders().putSingle(ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    responseContext.getHeaders().putSingle(ACCESS_CONTROL_ALLOW_HEADERS, "origin, content-type, accept, authorization");
+    responseContext.getHeaders().putSingle(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+    responseContext.getHeaders().putSingle(VARY, ORIGIN);
   }
 
-  protected void preflight(String origin, ContainerRequestContext requestContext) throws IOException
-  {
+  protected void preflight(String origin, ContainerRequestContext requestContext) {
     // Respond with a 204 no content since we are returning an empty response with just the header for preflight
     Response.ResponseBuilder builder = Response.noContent();
-    builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
-    builder.header(CorsHeaders.VARY, CorsHeaders.ORIGIN);
-    builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-    builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "origin, content-type, accept, authorization");
-    builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+    builder.header(ACCESS_CONTROL_ALLOW_ORIGIN, origin);
+    builder.header(VARY, ORIGIN);
+    builder.header(ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    builder.header(ACCESS_CONTROL_ALLOW_HEADERS, "origin, content-type, accept, authorization");
+    builder.header(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
     requestContext.abortWith(builder.build());
-
   }
 }
