1deb8e45cadf900cc9f0f062ceab851dd9a9a253
Bertil Chapuis <bchapuis@gmail.com>
2021-08-19 13:29:27 +0200

    Fix ServletConfig warning

---
+++ baremaps-openapi/src/main/java/com/baremaps/openapi/resources/ApiListingResource.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2020 The Baremaps Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.baremaps.openapi.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.models.Swagger;
+import io.swagger.util.Yaml;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+@Path("")
+public class ApiListingResource {
+
+  private final Swagger swagger;
+
+  public ApiListingResource() {
+    BeanConfig beanConfig = new BeanConfig();
+    beanConfig.setVersion("1.0.0");
+    beanConfig.setSchemes(new String[] {"http"});
+    beanConfig.setHost("localhost:9000");
+    beanConfig.setBasePath("/");
+    beanConfig.setResourcePackage("com.baremaps.openapi.services");
+    beanConfig.setScan(true);
+    this.swagger = beanConfig.getSwagger();
+  }
+
+  @GET
+  @Produces({"application/json"})
+  @Path("/swagger.json")
+  public Response getListingJson() {
+    return Response.ok(this.swagger).build();
+  }
+
+  @GET
+  @Produces({"application/yaml"})
+  @Path("/swagger.yaml")
+  public Response getListingYaml() throws JsonProcessingException {
+    String yaml = Yaml.mapper().writeValueAsString(this.swagger);
+    return Response.ok(yaml).build();
+  }
+}
