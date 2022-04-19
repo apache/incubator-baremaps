dbc654e4750b83b0419a52aae2077f7590bf383f
Drabble <antoine.drabble@gmail.com>
2022-04-19 16:29:23 +0200

    Unzip fetched file in the NicFetcher

--- baremaps-iploc/src/main/java/com/baremaps/iploc/nic/NicFetcher.java
+++ baremaps-iploc/src/main/java/com/baremaps/iploc/nic/NicFetcher.java
@@ -14,16 +14,14 @@
 
 package com.baremaps.iploc.nic;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.stream.Stream;
+import java.util.zip.GZIPInputStream;
 
 /** A utility for fetching the data of Network Coordination Centers (NIC) in temporary files. */
 public class NicFetcher {
@@ -31,29 +29,29 @@ public class NicFetcher {
   /** A list of default urls. */
   public static final List<String> NIC_URLS =
       List.of(
-          "https://ftp.afrinic.net/pub/dbase/afrinic.db.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.as-block.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.as-set.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.domain.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.filter-set.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.inet-rtr.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.inet6num.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.inetnum.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.irt.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.key-cert.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.limerick.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.mntner.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.organisation.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.peering-set.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.role.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.route-set.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.route.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.route6.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.rtr-set.gz",
-          "https://ftp.arin.net/pub/rr/arin.db.gz",
-          "https://ftp.arin.net/pub/rr/arin-nonauth.db.gz",
-          "https://ftp.lacnic.net/lacnic/dbase/lacnic.db.gz",
-          "https://ftp.ripe.net/ripe/dbase/ripe.db.gz");
+              "https://ftp.afrinic.net/pub/dbase/afrinic.db.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.as-block.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.as-set.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.domain.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.filter-set.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.inet-rtr.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.inet6num.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.inetnum.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.irt.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.key-cert.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.limerick.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.mntner.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.organisation.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.peering-set.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.role.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.route-set.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.route.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.route6.gz",
+              "https://ftp.apnic.net/apnic/whois/apnic.db.rtr-set.gz",
+              "https://ftp.arin.net/pub/rr/arin.db.gz",
+              "https://ftp.arin.net/pub/rr/arin-nonauth.db.gz",
+              "https://ftp.lacnic.net/lacnic/dbase/lacnic.db.gz",
+              "https://ftp.ripe.net/ripe/dbase/ripe.db.gz");
 
   /**
    * Fetches the default urls.
@@ -85,8 +83,10 @@ public class NicFetcher {
     try (InputStream inputStream = new BufferedInputStream(new URL(url).openStream())) {
       File file = File.createTempFile("nic_", ".download", new File("."));
       file.deleteOnExit();
-      long size = Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-      System.out.printf("Download %s (%smb)\n", url, size >> 20);
+      try (GZIPInputStream gis = new GZIPInputStream(inputStream)) {
+        long size = Files.copy(gis, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        System.out.printf("Download %s (%smb unzipped)\n", url,size >> 20);
+      }
       return file.toPath();
     } catch (Exception e) {
       throw new RuntimeException("Failed to download " + url);
