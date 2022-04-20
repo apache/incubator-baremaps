dea13501153a9f90f17f335b3663e3e8c6539de9
Drabble <antoine.drabble@gmail.com>
2022-04-20 09:17:23 +0200

    Use a logger instead of a printf

--- baremaps-iploc/src/main/java/com/baremaps/iploc/nic/NicFetcher.java
+++ baremaps-iploc/src/main/java/com/baremaps/iploc/nic/NicFetcher.java
@@ -14,6 +14,10 @@
 
 package com.baremaps.iploc.nic;
 
+import com.baremaps.iploc.database.InetnumLocationDaoSqliteImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
@@ -28,31 +32,11 @@ import java.util.stream.Stream;
 /** A utility for fetching the data of Network Coordination Centers (NIC) in temporary files. */
 public class NicFetcher {
 
+  private static final Logger logger = LoggerFactory.getLogger(NicFetcher.class);
+
   /** A list of default urls. */
   public static final List<String> NIC_URLS =
       List.of(
-          "https://ftp.afrinic.net/pub/dbase/afrinic.db.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.as-block.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.as-set.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.domain.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.filter-set.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.inet-rtr.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.inet6num.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.inetnum.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.irt.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.key-cert.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.limerick.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.mntner.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.organisation.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.peering-set.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.role.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.route-set.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.route.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.route6.gz",
-          "https://ftp.apnic.net/apnic/whois/apnic.db.rtr-set.gz",
-          "https://ftp.arin.net/pub/rr/arin.db.gz",
-          "https://ftp.arin.net/pub/rr/arin-nonauth.db.gz",
-          "https://ftp.lacnic.net/lacnic/dbase/lacnic.db.gz",
           "https://ftp.ripe.net/ripe/dbase/ripe.db.gz");
 
   /**
@@ -86,8 +70,7 @@ public class NicFetcher {
       File file = File.createTempFile("nic_", ".download", new File("."));
       file.deleteOnExit();
       long size = Files.copy(inputStream, file.toPath(), StandardCopyOption.REPLACE_EXISTING);
-      // TODO: we should use the logger
-      System.out.printf("Download %s (%smb unzipped)\n", url, size >> 20);
+      logger.info(String.format("Download %s (%smb unzipped)", url, size >> 20));
       return file.toPath();
     } catch (Exception e) {
       throw new RuntimeException("Failed to download " + url);
