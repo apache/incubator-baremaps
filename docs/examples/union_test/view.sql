f1eafcfbaf7544d0194a8c7d00aaf2543a9e6f47
Léonard BESSEAU <lbesseau@pictet.com>
2022-10-23 01:42:59 +0200

    Adds simplified view

--- docs/examples/union_test/view.sql
+++ docs/examples/union_test/view.sql
@@ -3,3 +3,9 @@ CREATE MATERIALIZED VIEW osm_landuse_relations AS Select (ST_Dump(ST_Union(ST_Ma
 
 DROP Materialized View IF EXISTS osm_landuse_ways;
 CREATE MATERIALIZED VIEW osm_landuse_ways AS Select (ST_Dump(ST_Union(ST_MakeValid(osm_ways.geom)))).path[1] as id, '{"landuse": "forest"}'::jsonb as tags, (ST_Dump(ST_Union(ST_MakeValid(osm_ways.geom)))).geom as geom FROM osm_ways WHERE tags ? 'landuse';
+
+DROP Materialized View IF EXISTS osm_landuse_relations_simplified;
+CREATE MATERIALIZED VIEW osm_landuse_relations_simplified AS Select (ST_Dump(st_simplifypreservetopology(ST_Union(ST_MakeValid(geom)), 39135 / power(2, 7)))).path[1] as id, '{"type": "multipolygon", "landuse": "forest"}'::jsonb as tags, (ST_Dump(st_simplifypreservetopology(ST_Union(ST_MakeValid(geom)), 39135 / power(2, 7)))).geom as geom FROM osm_relations WHERE tags ? 'landuse' AND tags ->> 'type' = 'multipolygon';
+
+DROP Materialized View IF EXISTS osm_landuse_ways_simplified;
+CREATE MATERIALIZED VIEW osm_landuse_ways_simplified AS Select (ST_Dump(st_simplifypreservetopology(ST_Union(ST_MakeValid(geom)), 39135 / power(2, 7)))).path[1] as id, '{"type": "multipolygon", "landuse": "forest"}'::jsonb as tags, (ST_Dump(st_simplifypreservetopology(ST_Union(ST_MakeValid(geom)), 39135 / power(2, 7)))).geom as geom FROM osm_ways WHERE tags ? 'landuse' ;
