55c76a984eb69a760435ec5c16ed56ca30a1e98c
Léonard BESSEAU <lbesseau@pictet.com>
2022-10-23 17:11:14 +0200

    Adds usage selection for landuse

--- docs/examples/union_test/view.sql
+++ docs/examples/union_test/view.sql
@@ -9,3 +9,17 @@ CREATE MATERIALIZED VIEW osm_landuse_relations_simplified AS Select (ST_Dump(st_
 
 DROP Materialized View IF EXISTS osm_landuse_ways_simplified;
 CREATE MATERIALIZED VIEW osm_landuse_ways_simplified AS Select (ST_Dump(st_simplifypreservetopology(ST_Union(ST_MakeValid(geom)), 39135 / power(2, 7)))).path[1] as id, '{"type": "multipolygon", "landuse": "forest"}'::jsonb as tags, (ST_Dump(st_simplifypreservetopology(ST_Union(ST_MakeValid(geom)), 39135 / power(2, 7)))).geom as geom FROM osm_ways WHERE tags ? 'landuse' ;
+
+
+DROP Materialized View IF EXISTS osm_landuse_relations_tags;
+CREATE MATERIALIZED VIEW osm_landuse_relations_tags AS SELECT ROW_NUMBER() OVER (ORDER BY tags -> 'landuse') as id, jsonb_build_object('landuse', tags -> 'landuse') as tags,  ST_Union(ST_MakeValid(osm_relations.geom)) as geom FROM osm_relations WHERE tags ? 'landuse' AND tags ->> 'type' = 'multipolygon' Group By (tags -> 'landuse');
+
+
+DROP Materialized View IF EXISTS osm_landuse_ways_tags;
+CREATE MATERIALIZED VIEW osm_landuse_ways_tags AS SELECT ROW_NUMBER() OVER (ORDER BY tags -> 'landuse') as id, jsonb_build_object('landuse', tags -> 'landuse') as tags,  ST_Union(ST_MakeValid(osm_ways.geom)) as geom FROM osm_ways WHERE tags ? 'landuse' Group By (tags -> 'landuse');
+
+DROP Materialized View IF EXISTS osm_landuse_relations_tags_2;
+CREATE MATERIALIZED VIEW osm_landuse_relations_tags_2 AS SELECT (ST_Dump(geom)).path[1] as id , tags, st_simplifypreservetopology((ST_Dump(geom)).geom , 39135 / power(2, 7)) as geom FROM osm_landuse_relations_tags;
+
+DROP Materialized View IF EXISTS osm_landuse_ways_tags_2;
+CREATE MATERIALIZED VIEW osm_landuse_ways_tags_2 AS SELECT (ST_Dump(geom)).path[1] as id , tags, st_simplifypreservetopology((ST_Dump(geom)).geom, 39135 / power(2, 7)) as geom FROM osm_landuse_ways_tags;
