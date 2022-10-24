0db20ae5f881a652599018b5ba18ed46c779a052
Léonard BESSEAU <lbesseau@pictet.com>
2022-10-24 22:36:56 +0200

    Updates view with more faster to produce version

--- docs/examples/union_test/view.sql
+++ docs/examples/union_test/view.sql
@@ -24,8 +24,24 @@ CREATE MATERIALIZED VIEW osm_landuse_relations_tags_2 AS SELECT (ST_Dump(geom)).
 DROP Materialized View IF EXISTS osm_landuse_ways_tags_2;
 CREATE MATERIALIZED VIEW osm_landuse_ways_tags_2 AS SELECT (ST_Dump(geom)).path[1] as id , tags, st_simplifypreservetopology((ST_Dump(geom)).geom, 39135 / power(2, 7)) as geom FROM osm_landuse_ways_tags;
 
-DROP Materialized View IF EXISTS osm_landuse_relations_tags_3;
-CREATE MATERIALIZED VIEW osm_landuse_relations_tags_3 AS
+
+-- DROP Materialized View IF EXISTS osm_landuse_relations_tags_3;
+--CREATE MATERIALIZED VIEW osm_landuse_relations_tags_3 AS
+--With test_polygons_1 as (
+--Select tags, (ST_DUMP(geom)).geom as geom FROM osm_landuse_relations_tags WHERE ST_GeometryType(geom) = 'ST_MultiPolygon'
+--Union ALL
+--Select tags, ST_MakeValid(geom) FROM osm_landuse_relations_tags WHERE ST_GeometryType(geom) = 'ST_Polygon'
+--UNION ALL
+--SELECT tags ,ST_CollectionExtract(geom, 3) as geom  FROM  osm_landuse_relations_tags WHERE ST_GeometryType(geom) = 'ST_GeometryCollection')
+--,
+--test_polygons_2 as (
+--Select tags, (ST_DUMP(geom)).geom as geom FROM test_polygons_1 WHERE ST_GeometryType(geom) = 'ST_MultiPolygon'
+--Union ALL
+--Select tags, ST_MakeValid(geom) FROM test_polygons_1 WHERE ST_GeometryType(geom) = 'ST_Polygon')
+--Select ROW_NUMBER() OVER (ORDER BY (ST_DUMP(ST_union(geom))).geom) as id,  (array_agg(tags))[1] as tags, (ST_DUMP(ST_union(geom))).geom as geom from ( Select tags, ST_BuildArea((ST_DumpRings(geom)).geom) as geom from test_polygons_2) as data GROUP BY tags
+
+DROP Materialized View IF EXISTS osm_landuse_relations_tags_4;
+CREATE MATERIALIZED VIEW osm_landuse_relations_tags_4 AS
 With test_polygons_1 as (
 Select tags, (ST_DUMP(geom)).geom as geom FROM osm_landuse_relations_tags WHERE ST_GeometryType(geom) = 'ST_MultiPolygon'
 Union ALL
@@ -37,4 +53,4 @@ test_polygons_2 as (
 Select tags, (ST_DUMP(geom)).geom as geom FROM test_polygons_1 WHERE ST_GeometryType(geom) = 'ST_MultiPolygon'
 Union ALL
 Select tags, ST_MakeValid(geom) FROM test_polygons_1 WHERE ST_GeometryType(geom) = 'ST_Polygon')
-Select ROW_NUMBER() OVER (ORDER BY (ST_DUMP(ST_union(geom))).geom) as id,  (array_agg(tags))[1] as tags, (ST_DUMP(ST_union(geom))).geom as geom from ( Select tags, ST_BuildArea((ST_DumpRings(geom)).geom) as geom from test_polygons_2) as data GROUP BY tags
+Select ROW_NUMBER() OVER (ORDER BY (ST_DUMP(ST_union(geom))).geom) as id,  (array_agg(tags))[1] as tags, (ST_DUMP(ST_union(geom))).geom as geom from ( Select tags, ST_BuildArea(ST_ExteriorRing(geom)) as geom from test_polygons_2) as data GROUP BY tags
