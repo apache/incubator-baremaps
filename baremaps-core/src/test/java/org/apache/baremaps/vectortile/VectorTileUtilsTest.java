498c5c3076d8b0351373f7e45cccb5662c19c382
Bertil Chapuis <bchapuis@gmail.com>
2023-03-26 12:29:12 +0200

    Implement the vector tile specification

---
+++ baremaps-core/src/test/java/org/apache/baremaps/vectortile/VectorTileUtilsTest.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.apache.baremaps.vectortile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.locationtech.jts.geom.*;
+
+class VectorTileUtilsTest {
+
+  @Test
+  void asMvtGeom() {
+    // Create a test geometry (a simple square)
+    var coordinates = new Coordinate[] {
+        new Coordinate(1, 1),
+        new Coordinate(5, 9),
+        new Coordinate(9, 1),
+        new Coordinate(1, 1)
+    };
+    var geometryFactory = new GeometryFactory();
+    var inputGeom = geometryFactory.createPolygon(coordinates);
+
+    // Define the tile envelope, extent, buffer, and clipping flag
+    var envelope = new Envelope(0, 10, 0, 10);
+    var extent = 100;
+    var buffer = 10;
+    var clipGeom = true;
+
+    // Transform the input geometry using asMvtGeom
+    var outputGeom =
+        VectorTileUtils.asVectorTileGeom(inputGeom, envelope, extent, buffer, clipGeom);
+
+    // Check if the output geometry is not null
+    assertNotNull(outputGeom);
+
+    // Check if the output geometry is a valid Geometry
+    assertTrue(outputGeom.isValid());
+
+    // Define expected coordinates for the transformed geometry
+    Coordinate[] expectedCoordinates = new Coordinate[] {
+        new Coordinate(10, 90),
+        new Coordinate(90, 90),
+        new Coordinate(50, 10),
+        new Coordinate(10, 90)
+    };
+
+    // Compare the transformed geometry with the expected geometry
+    LinearRing expectedShell = geometryFactory.createLinearRing(expectedCoordinates);
+    Polygon expectedGeom = geometryFactory.createPolygon(expectedShell);
+    assertTrue(outputGeom.equalsTopo(expectedGeom));
+
+  }
+}
