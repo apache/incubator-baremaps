d10756b3581e46d4003650d2d1ab2ecbd5c3a3cd
Bertil Chapuis <bchapuis@gmail.com>
2023-03-31 18:12:57 +0200

    Implement the vector tile specification (#617)

---
+++ baremaps-core/src/main/java/org/apache/baremaps/vectortile/VectorTileDecoder.java
@@ -0,0 +1,425 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.apache.baremaps.vectortile;
+
+import static org.apache.baremaps.vectortile.VectorTileUtils.*;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.*;
+import java.util.stream.Collectors;
+import org.apache.baremaps.mvt.binary.VectorTile;
+import org.apache.baremaps.mvt.binary.VectorTile.Tile.GeomType;
+import org.apache.baremaps.mvt.binary.VectorTile.Tile.Value;
+import org.locationtech.jts.geom.*;
+
+/**
+ * A vector tile decoder.
+ *
+ * This implementation is based on the Vector Tile Specification.
+ */
+public class VectorTileDecoder {
+  private static final GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
+
+  private int cx = 0;
+
+  private int cy = 0;
+
+  private List<String> keys = new ArrayList<>();
+
+  private List<Object> values = new ArrayList<>();
+
+  /**
+   * Constructs a new vector tile decoder.
+   */
+  public VectorTileDecoder() {}
+
+  /**
+   * Decodes a vector tile.
+   *
+   * @param buffer The bytes of the vector tile
+   * @return The decoded vector tile
+   * @throws IOException If an error occurs while decoding the vector tile
+   */
+  public Tile decodeTile(ByteBuffer buffer) throws IOException {
+    try {
+      VectorTile.Tile tile = VectorTile.Tile.parseFrom(buffer);
+      return decodeTile(tile);
+    } catch (InvalidProtocolBufferException e) {
+      throw new IOException(e);
+    }
+  }
+
+  /**
+   * Decodes a vector tile.
+   *
+   * @param tile The vector tile to decode
+   * @return The decoded vector tile
+   */
+  public Tile decodeTile(VectorTile.Tile tile) {
+    List<Layer> layers = tile.getLayersList().stream()
+        .map(this::decodeLayer)
+        .collect(Collectors.toList());
+    return new Tile(layers);
+  }
+
+  /**
+   * Decodes a vector tile layer.
+   *
+   * @param buffer The bytes of the vector tile layer
+   * @return The decoded layer
+   * @throws IOException If an error occurs while decoding the vector tile layer
+   */
+  public Layer decodeLayer(ByteBuffer buffer) throws IOException {
+    try {
+      VectorTile.Tile.Layer layer = VectorTile.Tile.Layer.parseFrom(buffer);
+      return decodeLayer(layer);
+    } catch (InvalidProtocolBufferException e) {
+      throw new IOException(e);
+    }
+  }
+
+  /**
+   * Decodes a vector tile layer.
+   *
+   * @param layer The vector tile layer
+   * @return The decoded layer
+   */
+  public Layer decodeLayer(VectorTile.Tile.Layer layer) {
+    String name = layer.getName();
+    int extent = layer.getExtent();
+
+    cx = 0;
+    cy = 0;
+
+    keys = layer.getKeysList();
+    values = layer.getValuesList().stream()
+        .map(this::decodeValue)
+        .collect(Collectors.toList());
+
+    List<Feature> features = layer.getFeaturesList().stream()
+        .map(this::decodeFeature)
+        .collect(Collectors.toList());
+
+    return new Layer(name, extent, features);
+  }
+
+  /**
+   * Decodes a vector tile value into a Java object.
+   *
+   * @param value The vector tile value
+   * @return The Java object
+   */
+  protected Object decodeValue(Value value) {
+    if (value.hasStringValue()) {
+      return value.getStringValue();
+    } else if (value.hasFloatValue()) {
+      return value.getFloatValue();
+    } else if (value.hasDoubleValue()) {
+      return value.getDoubleValue();
+    } else if (value.hasIntValue()) {
+      return value.getIntValue();
+    } else if (value.hasSintValue()) {
+      return value.getSintValue();
+    } else if (value.hasUintValue()) {
+      return value.getUintValue();
+    } else if (value.hasBoolValue()) {
+      return value.getBoolValue();
+    } else {
+      throw new IllegalStateException("Value is not set.");
+    }
+  }
+
+  /**
+   * Decodes a feature from a vector tile.
+   * 
+   * @param feature The vector tile feature to decode
+   * @return The decoded feature
+   */
+  protected Feature decodeFeature(VectorTile.Tile.Feature feature) {
+
+    cx = 0;
+    cy = 0;
+
+    long id = feature.getId();
+    Map<String, Object> tags = decodeTags(feature);
+    Geometry geometry = decodeGeometry(feature);
+    return new Feature(id, tags, geometry);
+  }
+
+  /**
+   * Decodes the tags from a vector tile feature.
+   *
+   * @param feature The feature to decode
+   * @return The tags of the feature
+   */
+  protected Map<String, Object> decodeTags(VectorTile.Tile.Feature feature) {
+    Map<String, Object> tags = new HashMap<>();
+    List<Integer> encoding = feature.getTagsList();
+    for (int i = 0; i < encoding.size(); i += 2) {
+      int key = encoding.get(i);
+      int value = encoding.get(i + 1);
+      tags.put(keys.get(key), values.get(value));
+    }
+    return tags;
+  }
+
+  /**
+   * Decodes a geometry from a vector tile feature.
+   *
+   * @param feature The vector tile feature
+   * @return The decoded geometry
+   */
+  protected Geometry decodeGeometry(VectorTile.Tile.Feature feature) {
+    GeomType type = feature.getType();
+    List<Integer> encoding = feature.getGeometryList();
+    switch (type) {
+      case POINT:
+        return decodePoint(encoding);
+      case LINESTRING:
+        return decodeLineString(encoding);
+      case POLYGON:
+        return decodePolygon(encoding);
+      case UNKNOWN:
+      default:
+        throw new IllegalStateException("Unknown geometry type.");
+    }
+  }
+
+  /**
+   * Decode a point geometry.
+   *
+   * @param encoding The encoding of the point geometry
+   * @return The decoded point geometry
+   */
+  protected Geometry decodePoint(List<Integer> encoding) {
+    List<Coordinate> coordinates = new ArrayList<>();
+
+    // Iterate over the commands and parameters
+    int i = 0;
+    while (i < encoding.size()) {
+      int value = encoding.get(i);
+      int command = command(value);
+      int count = count(value);
+
+      // Increment the index to the first parameter
+      i++;
+
+      // Iterate over the parameters
+      int length = count * 2;
+      for (int j = 0; j < length; j += 2) {
+        // Decode the parameters and move the cursor
+        cx += parameter(encoding.get(i + j));
+        cy += parameter(encoding.get(i + j + 1));
+
+        // Add the coordinate to the list
+        if (command == MOVE_TO) {
+          coordinates.add(new Coordinate(cx, cy));
+        }
+      }
+
+      // Increment the index to the next command
+      i += length;
+    }
+
+    // Build the final geometry
+    if (coordinates.size() == 1) {
+      return GEOMETRY_FACTORY.createPoint(coordinates.get(0));
+    } else if (coordinates.size() > 1) {
+      return GEOMETRY_FACTORY.createMultiPointFromCoords(coordinates.toArray(new Coordinate[0]));
+    } else {
+      throw new IllegalStateException("No coordinates found.");
+    }
+  }
+
+  /**
+   * Decode a line string.
+   *
+   * @param encoding The encoding of the line string
+   * @return The decoded line string
+   */
+  protected Geometry decodeLineString(List<Integer> encoding) {
+    List<LineString> lineStrings = new ArrayList<>();
+    List<Coordinate> coordinates = new ArrayList<>();
+
+    // Iterate over the commands and parameters
+    int i = 0;
+    while (i < encoding.size()) {
+      int value = encoding.get(i);
+      int command = command(value);
+      int count = count(value);
+
+      // Increment the index to the first parameter
+      i++;
+
+      // Iterate over the parameters
+      int length = count * 2;
+      for (int j = 0; j < length; j += 2) {
+        // Decode the parameters and move the cursor
+        cx += parameter(encoding.get(i + j));
+        cy += parameter(encoding.get(i + j + 1));
+
+        // Start a new linestring
+        if (command == MOVE_TO) {
+          coordinates.clear();
+          coordinates.add(new Coordinate(cx, cy));
+        }
+
+        // Add the coordinate to the current linestring
+        else if (command == LINE_TO) {
+          coordinates.add(new Coordinate(cx, cy));
+        }
+      }
+
+      // Add the linestring to the list of linestrings
+      if (coordinates.size() > 1) {
+        lineStrings.add(GEOMETRY_FACTORY.createLineString(coordinates.toArray(new Coordinate[0])));
+      }
+
+      // Increment the index to the next command
+      i += length;
+    }
+
+    // Build the final geometry
+    if (lineStrings.size() == 1) {
+      return lineStrings.get(0);
+    } else if (lineStrings.size() > 1) {
+      return GEOMETRY_FACTORY.createMultiLineString(lineStrings.toArray(new LineString[0]));
+    } else {
+      throw new IllegalStateException("No linestrings found.");
+    }
+  }
+
+  /**
+   * Decodes a polygon geometry.
+   *
+   * @param encoding The encoding of the polygon
+   * @return The geometry
+   */
+  protected Geometry decodePolygon(List<Integer> encoding) {
+    List<Polygon> polygons = new ArrayList<>();
+    Optional<LinearRing> shell = Optional.empty();
+    List<LinearRing> holes = new ArrayList<>();
+    List<Coordinate> coordinates = new ArrayList<>();
+
+    // Iterate over the commands and parameters
+    int i = 0;
+    while (i < encoding.size()) {
+      int value = encoding.get(i);
+      int command = command(value);
+      int count = count(value);
+
+      // Accumulate the coordinates
+      if (command == MOVE_TO || command == LINE_TO) {
+
+        // Increment the index to the first parameter
+        i++;
+
+        int length = count * 2;
+        for (int j = 0; j < length; j += 2) {
+          // Decode the parameters and move the cursor
+          cx += parameter(encoding.get(i + j));
+          cy += parameter(encoding.get(i + j + 1));
+
+          // Start a new linear ring
+          if (command == MOVE_TO) {
+            coordinates.clear();
+            coordinates.add(new Coordinate(cx, cy));
+          }
+
+          // Add the coordinate to the current linear ring
+          else if (command == LINE_TO) {
+            coordinates.add(new Coordinate(cx, cy));
+          }
+        }
+
+        // Increment the index to the next command
+        i += length;
+      }
+
+      // Assemble the linear rings
+      if (command == CLOSE_PATH) {
+        coordinates.add(coordinates.get(0));
+        LinearRing linearRing =
+            GEOMETRY_FACTORY.createLinearRing(coordinates.toArray(new Coordinate[0]));
+        boolean isShell = isClockWise(linearRing);
+
+        // Build the previous polygon
+        if (isShell && shell.isPresent()) {
+          polygons
+              .add(GEOMETRY_FACTORY.createPolygon(shell.get(), holes.toArray(new LinearRing[0])));
+          holes.clear();
+        }
+
+        // Add the linear ring to the appropriate variable
+        if (isShell) {
+          shell = Optional.of(linearRing);
+        } else {
+          holes.add(linearRing);
+        }
+
+        // Reset the coordinates
+        coordinates.clear();
+
+        // Increment the index to the next command
+        i++;
+      }
+    }
+
+    // Build the last polygon
+    if (shell.isPresent()) {
+      polygons.add(GEOMETRY_FACTORY.createPolygon(shell.get(), holes.toArray(new LinearRing[0])));
+      holes.clear();
+    }
+
+    // Build the final geometry
+    if (polygons.size() == 1) {
+      return polygons.get(0);
+    } else if (polygons.size() > 1) {
+      return GEOMETRY_FACTORY.createMultiPolygon(polygons.toArray(new Polygon[0]));
+    } else {
+      throw new IllegalStateException("No polygons found.");
+    }
+  }
+
+  /**
+   * Returns the command for the given value.
+   *
+   * @param value The value
+   * @return The command
+   */
+  protected int command(int value) {
+    return value & 0x7;
+  }
+
+  /**
+   * Returns the number of parameters for the given value.
+   *
+   * @param value The value
+   * @return The number of parameters
+   */
+  protected int count(int value) {
+    return value >> 3;
+  }
+
+  /**
+   * Decodes a parameter from the given value.
+   *
+   * @param value The value to decode
+   * @return The decoded parameter
+   */
+  protected Integer parameter(int value) {
+    return (value >> 1) ^ (-(value & 1));
+  }
+}
---
+++ baremaps-core/src/test/java/org/apache/baremaps/vectortile/VectorTileViewer.java
@@ -0,0 +1,153 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.apache.baremaps.vectortile;
+
+import java.awt.*;
+import java.awt.Dimension;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.zip.GZIPInputStream;
+import javax.swing.*;
+import org.locationtech.jts.geom.*;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.Polygon;
+
+/**
+ * A vector tile viewer.
+ */
+public class VectorTileViewer {
+
+  public static void main(String... args) throws Exception {
+    String arg =
+        args.length > 0 ? args[0] : "baremaps-core/src/test/resources/vectortile/14-8493-5795.mvt";
+    var path = Path.of(arg);
+    try (var input = new GZIPInputStream(Files.newInputStream(path))) {
+      var buffer = ByteBuffer.wrap(input.readAllBytes());
+      var parsed = org.apache.baremaps.mvt.binary.VectorTile.Tile.parseFrom(buffer);
+      var tile = new VectorTileDecoder().decodeTile(parsed);
+      JFrame f = new JFrame("Vector Tile Viewer");
+      f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      f.add(new TilePanel(tile, 1000));
+      f.pack();
+      f.setVisible(true);
+    }
+  }
+
+  static class TilePanel extends JPanel {
+
+    private final Tile tile;
+
+    private final int extent;
+
+    public TilePanel(Tile tile, int extent) {
+      this.tile = tile;
+      this.extent = extent;
+    }
+
+    public Dimension getPreferredSize() {
+      return new Dimension(extent, extent);
+    }
+
+    public void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      graphics.setColor(Color.black);
+
+      Graphics2D graphics2D = (Graphics2D) graphics;
+      graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+          RenderingHints.VALUE_ANTIALIAS_ON);
+      graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+          RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+      for (var layer : tile.getLayers()) {
+        for (var feature : layer.getFeatures()) {
+          var geometry = feature.getGeometry();
+          paintGeometry(graphics, geometry, layer.getExtent());
+        }
+      }
+    }
+
+    public void paintGeometry(Graphics graphics, Geometry geometry, int extent) {
+      switch (geometry.getGeometryType()) {
+        case "Point":
+          paintPoint(graphics, (Point) geometry, extent);
+          break;
+        case "LineString":
+          paintLineString(graphics, (LineString) geometry, extent);
+          break;
+        case "Polygon":
+          paintPolygon(graphics, (Polygon) geometry, extent);
+          break;
+        case "MultiPoint":
+        case "MultiLineString":
+        case "MultiPolygon":
+        case "GeometryCollection":
+          paintGeometryCollection(graphics, (GeometryCollection) geometry, extent);
+          break;
+        default:
+          throw new IllegalArgumentException(
+              "Unknown geometry type: " + geometry.getGeometryType());
+      }
+    }
+
+    private void paintGeometryCollection(Graphics graphics, GeometryCollection geometryCollection,
+        int extent) {
+      for (int i = 0; i < geometryCollection.getNumGeometries(); i++) {
+        paintGeometry(graphics, geometryCollection.getGeometryN(i), extent);
+      }
+    }
+
+    private void paintPolygon(Graphics graphics, Polygon polygon, int extent) {
+      paintLinearRing(graphics, polygon.getExteriorRing(), extent);
+      for (int i = 0; i < polygon.getNumInteriorRing(); i++) {
+        paintLinearRing(graphics, polygon.getInteriorRingN(i), extent);
+      }
+    }
+
+    private void paintLinearRing(Graphics graphics, LinearRing polygon, int extent) {
+      Coordinate[] coordinates = polygon.getCoordinates();
+      for (int i = 0; i < coordinates.length - 1; i++) {
+        var c1 = coordinates[i];
+        var c2 = coordinates[i + 1];
+        var x1 = coord(c1.x, extent, getWidth());
+        var y1 = coord(c1.y, extent, getHeight());
+        var x2 = coord(c2.x, extent, getWidth());
+        var y2 = coord(c2.y, extent, getHeight());
+        graphics.drawLine(x1, y1, x2, y2);
+      }
+    }
+
+    private void paintLineString(Graphics graphics, LineString lineString, int extent) {
+      Coordinate[] coordinates = lineString.getCoordinates();
+      for (int i = 0; i < coordinates.length - 1; i++) {
+        var c1 = coordinates[i];
+        var c2 = coordinates[i + 1];
+        var x1 = coord(c1.x, extent, getWidth());
+        var y1 = coord(c1.y, extent, getHeight());
+        var x2 = coord(c2.x, extent, getWidth());
+        var y2 = coord(c2.y, extent, getHeight());
+        graphics.drawLine(x1, y1, x2, y2);
+      }
+    }
+
+    private void paintPoint(Graphics graphics, Point point, int extent) {
+      var x = coord(point.getX(), extent, getWidth());
+      var y = coord(point.getY(), extent, getHeight());
+      graphics.fillOval(x - 2, y - 2, 4, 4);
+    }
+
+    private int coord(double value, int extent, int size) {
+      return (int) (value * size / extent);
+    }
+  }
+}
