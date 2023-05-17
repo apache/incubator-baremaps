5e7b73433f3ad621947b6045a776ba71b87bf793
Perdjesk <Perdjesk@users.noreply.github.com>
2023-05-17 16:31:27 +0200

    Support JS tileset file in CLI map dev

--- baremaps-cli/src/main/java/org/apache/baremaps/cli/map/Dev.java
+++ baremaps-cli/src/main/java/org/apache/baremaps/cli/map/Dev.java
@@ -22,6 +22,7 @@ import java.nio.file.Path;
 import java.util.concurrent.Callable;
 import javax.sql.DataSource;
 import org.apache.baremaps.cli.Options;
+import org.apache.baremaps.config.ConfigReader;
 import org.apache.baremaps.postgres.PostgresUtils;
 import org.apache.baremaps.server.CorsFilter;
 import org.apache.baremaps.server.DevResources;
@@ -60,10 +61,11 @@ public class Dev implements Callable<Integer> {
 
   @Override
   public Integer call() throws Exception {
+    var configReader = new ConfigReader();
     // Configure serialization
     var objectMapper = objectMapper();
 
-    var tileSet = objectMapper.readValue(tileset.toFile(), Tileset.class);
+    var tileSet = objectMapper.readValue(configReader.read(tileset), Tileset.class);
     var database = tileSet.getDatabase();
 
     try (var dataSource = PostgresUtils.dataSource(database)) {
