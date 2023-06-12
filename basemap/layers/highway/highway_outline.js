c47693368a1236f4b838f2c92130c88f489dac5e
MaximeLesti <maxime.lestiboudois@gmail.com>
2023-06-12 12:02:44 +0200

    Fix importations

--- basemap/layers/highway/highway_outline.js
+++ basemap/layers/highway/highway_outline.js
@@ -10,7 +10,7 @@
  the License.
  **/
 import {asLayerObject, withSortKeys} from "../../utils/utils.js";
-import colorScheme from "../../colorScheme.js";
+import colorScheme from "../../theme.js";
 
 let directives =[
     {
