fe5a444799cd5e3bd3cd7fe9e9016104df8c28fa
Bertil Chapuis <bchapuis@gmail.com>
2023-08-18 15:12:30 +0200

    Display natural layer at low zoom levels

--- basemap/layers/landuse/simplify.sql
+++ basemap/layers/landuse/simplify.sql
@@ -33,97 +33,61 @@ CREATE VIEW osm_landuse_z13 AS
 SELECT id, tags, geom FROM osm_landuse;
 
 CREATE MATERIALIZED VIEW osm_landuse_z12 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 12)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 12)) AS geom
 FROM osm_landuse
 WHERE st_area(st_envelope(geom)) > 25 * power(78270 / power(2, 12), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z11 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 11)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 11)) AS geom
 FROM osm_landuse
 WHERE st_area(st_envelope(geom)) > 25 * power(78270 / power(2, 11), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z10 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 10)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 10)) AS geom
 FROM osm_landuse
 WHERE st_area(st_envelope(geom)) > 25 * power(78270 / power(2, 10), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z9 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 9)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 9)) AS geom
 FROM osm_landuse
 WHERE st_area(st_envelope(geom)) > 25 * power(78270 / power(2, 9), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z8 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 8)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 8)) AS geom
 FROM osm_landuse_xl
 WHERE st_area(st_envelope(geom)) > 25 * power(78270 / power(2, 8), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z7 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 7)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 7)) AS geom
 FROM osm_landuse_l
 WHERE st_area(st_envelope(geom)) > 150 * power(78270 / power(2, 7), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z6 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 6)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 6)) AS geom
 FROM osm_landuse_m
 WHERE st_area(st_envelope(geom)) > 150 * power(78270 / power(2, 6), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z5 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 5)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 5)) AS geom
 FROM osm_landuse_s
 WHERE st_area(st_envelope(geom)) > 150 * power(78270 / power(2, 5), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z4 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 4)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 4)) AS geom
 FROM osm_landuse_s
 WHERE st_area(st_envelope(geom)) > 150 * power(78270 / power(2, 4), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z3 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 3)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 3)) AS geom
 FROM osm_landuse_s
 WHERE st_area(st_envelope(geom)) > 150 * power(78270 / power(2, 3), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z2 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 2)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 2)) AS geom
 FROM osm_landuse_s
 WHERE st_area(st_envelope(geom)) > 150 * power(78270 / power(2, 2), 2);
 
 CREATE MATERIALIZED VIEW osm_landuse_z1 AS
-SELECT
-    id,
-    tags,
-    st_simplifypreservetopology(geom, 78270 / power(2, 1)) AS geom
+SELECT id, tags, st_simplifypreservetopology(geom, 78270 / power(2, 1)) AS geom
 FROM osm_landuse_s
 WHERE st_area(st_envelope(geom)) > 250 * power(78270 / power(2, 1), 2);
