dbdaceea1f9f1a8fd29c66862be37d0c4221f39b
Bertil Chapuis <bchapuis@gmail.com>
2023-08-29 08:21:05 +0200

    Replace executeBatch by execute

--- baremaps-core/src/main/java/org/apache/baremaps/openstreetmap/postgres/PostgresRelationRepository.java
+++ baremaps-core/src/main/java/org/apache/baremaps/openstreetmap/postgres/PostgresRelationRepository.java
@@ -264,7 +264,7 @@ public class PostgresRelationRepository implements RelationRepository {
     try (Connection connection = dataSource.getConnection();
         PreparedStatement statement = connection.prepareStatement(deleteIn)) {
       statement.setArray(1, connection.createArrayOf("int8", keys.toArray()));
-      statement.executeBatch();
+      statement.execute();
     } catch (SQLException e) {
       throw new RepositoryException(e);
     }
--- baremaps-core/src/main/java/org/apache/baremaps/openstreetmap/postgres/PostgresWayRepository.java
+++ baremaps-core/src/main/java/org/apache/baremaps/openstreetmap/postgres/PostgresWayRepository.java
@@ -261,7 +261,7 @@ public class PostgresWayRepository implements WayRepository {
     try (Connection connection = dataSource.getConnection();
         PreparedStatement statement = connection.prepareStatement(deleteIn)) {
       statement.setArray(1, connection.createArrayOf("int8", keys.toArray()));
-      statement.executeBatch();
+      statement.execute();
     } catch (SQLException e) {
       throw new RepositoryException(e);
     }
