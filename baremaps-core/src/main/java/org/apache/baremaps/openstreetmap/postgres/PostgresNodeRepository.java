246e659c2374a7c0fe742d8f4c49da596a5bc691
Bertil Chapuis <bchapuis@gmail.com>
2023-08-29 08:23:15 +0200

    Use bigint instead of int8

--- baremaps-core/src/main/java/org/apache/baremaps/openstreetmap/postgres/PostgresNodeRepository.java
+++ baremaps-core/src/main/java/org/apache/baremaps/openstreetmap/postgres/PostgresNodeRepository.java
@@ -257,7 +257,7 @@ public class PostgresNodeRepository implements NodeRepository {
     }
     try (Connection connection = dataSource.getConnection();
         PreparedStatement statement = connection.prepareStatement(deleteIn)) {
-      statement.setArray(1, connection.createArrayOf("int8", keys.toArray()));
+      statement.setArray(1, connection.createArrayOf("bigint", keys.toArray()));
       statement.execute();
     } catch (SQLException e) {
       throw new RepositoryException(e);
