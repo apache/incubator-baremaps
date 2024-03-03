002345322e1cbe2f4288b7a0b19ed9dedf8a4a7c
Bertil Chapuis <bchapuis@gmail.com>
2024-03-03 09:24:18 +0100

    Add missing license headers (#839)
    * Add missing license headers
    
    * Add script to check license headers with rat

--- scripts/check-license-headers.sh
+++ scripts/check-license-headers.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
@@ -13,4 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-.level = WARNING
+DIR="$(dirname "$0")"
+cd "$DIR" || exit
+./mvnw clean && ./mvnw apache-rat:check
