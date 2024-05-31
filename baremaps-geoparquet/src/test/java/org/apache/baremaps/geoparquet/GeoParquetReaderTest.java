407ec3ebf803718b1d41c40d05d81714798b3348
sebr72 <sebastien_riollet@hotmail.com>
2024-05-31 02:25:20 +0200

    Add new assertion.

--- baremaps-geoparquet/src/test/java/org/apache/baremaps/geoparquet/GeoParquetReaderTest.java
+++ baremaps-geoparquet/src/test/java/org/apache/baremaps/geoparquet/GeoParquetReaderTest.java
@@ -17,26 +17,51 @@
 
 package org.apache.baremaps.geoparquet;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.baremaps.testing.TestFiles;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class GeoParquetReaderTest {
 
   @Test
   void read() throws IOException, URISyntaxException {
-    // URI geoParquet = new
-    // URI("s3a://overturemaps-us-west-2/release/2024-03-12-alpha.0/theme=admins/type=locality_area/*.parquet");
     URI geoParquet = TestFiles.GEOPARQUET.toUri();
+    final boolean isPrintingContent = true;
+    final int expectedGroupCount = 5;
 
+    readGroups(geoParquet, isPrintingContent, expectedGroupCount);
+  }
+
+  @Disabled("Takes too long. Around 10 minutes.")
+  @Test
+  void readExternal() throws IOException, URISyntaxException {
+    URI geoParquet = new URI(
+        "s3a://overturemaps-us-west-2/release/2024-03-12-alpha.0/theme=admins/type=locality_area/*.parquet");
+    final boolean isPrintingContent = false;
+    final int expectedGroupCount = 974708;
+
+    readGroups(geoParquet, isPrintingContent, expectedGroupCount);
+  }
+
+  private static void readGroups(URI geoParquet, boolean isPrintingContent, int expectedGroupCount)
+      throws IOException, URISyntaxException {
     GeoParquetReader geoParquetReader = new GeoParquetReader(geoParquet);
+    final AtomicInteger groupCount = new AtomicInteger();
     geoParquetReader.read().forEach(group -> {
-      System.out.println("-----");
-      System.out.println(group.getSchema());
-      System.out.println(group.getGeometryValue("geometry"));
-
+      groupCount.getAndIncrement();
+      if (isPrintingContent) {
+        System.out.println("-----");
+        System.out.println(group.getSchema());
+        System.out.println(group.getGeometryValue("geometry"));
+      }
     });
+
+    assertEquals(expectedGroupCount, groupCount.get());
   }
 }
