e4013c4ed76d05b9431380c897901666861d8cdc
Antoine Drabble <antoine.drabble@gmail.com>
2024-06-03 18:01:35 +0200

    Use the default CRS when crs field is null in Geoparquet (#861)

--- baremaps-geoparquet/src/main/java/org/apache/baremaps/geoparquet/data/GeoParquetMetadata.java
+++ baremaps-geoparquet/src/main/java/org/apache/baremaps/geoparquet/data/GeoParquetMetadata.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.base.Objects;
 import java.util.Map;
+import java.util.Optional;
 
 public class GeoParquetMetadata {
 
@@ -58,15 +59,17 @@ public class GeoParquetMetadata {
   }
 
   public int getSrid(String column) {
-    JsonNode crsId = getColumns().get(column).getCrs().get("id");
-    return switch (crsId.get("authority").asText()) {
-      case "OGC" -> switch (crsId.get("code").asText()) {
-          case "CRS84" -> 4326;
-          default -> 0;
-        };
-      case "EPSG" -> crsId.get("code").asInt();
-      default -> 0;
-    };
+    return Optional.ofNullable(getColumns().get(column).getCrs()).map(crs -> {
+      JsonNode id = crs.get("id");
+      return switch (id.get("authority").asText()) {
+        case "OGC" -> switch (id.get("code").asText()) {
+            case "CRS84" -> 4326;
+            default -> 0;
+          };
+        case "EPSG" -> id.get("code").asInt();
+        default -> 0;
+      };
+    }).orElse(4326);
   }
 
   public boolean isGeometryColumn(String column) {
