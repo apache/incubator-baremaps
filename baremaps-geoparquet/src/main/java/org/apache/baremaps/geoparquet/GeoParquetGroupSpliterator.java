21816d7a250bfa236b10df87080330012d63cd68
Bertil Chapuis <bchapuis@gmail.com>
2024-09-30 23:45:46 +0200

    Improve the geoparquet reader

--- baremaps-geoparquet/src/main/java/org/apache/baremaps/geoparquet/GeoParquetGroupSpliterator.java
+++ baremaps-geoparquet/src/main/java/org/apache/baremaps/geoparquet/GeoParquetGroupSpliterator.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.baremaps.geoparquet;
 
 import java.io.IOException;
@@ -22,10 +39,10 @@ class GeoParquetGroupSpliterator implements Spliterator<GeoParquetGroup> {
   }
 
   GeoParquetGroupSpliterator(
-          GeoParquetReader geoParquetReader,
-          List<FileStatus> fileStatuses,
-          int startIndex,
-          int endIndex) {
+      GeoParquetReader geoParquetReader,
+      List<FileStatus> fileStatuses,
+      int startIndex,
+      int endIndex) {
     this.geoParquetReader = geoParquetReader;
     this.fileStatuses = fileStatuses;
     this.currentFileIndex = startIndex;
@@ -87,10 +104,10 @@ class GeoParquetGroupSpliterator implements Spliterator<GeoParquetGroup> {
   }
 
   private ParquetReader<GeoParquetGroup> createParquetReader(FileStatus file)
-          throws IOException {
+      throws IOException {
     return ParquetReader.builder(new GeoParquetGroupReadSupport(), file.getPath())
-            .withConf(geoParquetReader.configuration)
-            .build();
+        .withConf(geoParquetReader.getConfiguration())
+        .build();
   }
 
   @Override
@@ -101,7 +118,7 @@ class GeoParquetGroupSpliterator implements Spliterator<GeoParquetGroup> {
     }
     int mid = currentFileIndex + remainingFiles / 2;
     GeoParquetGroupSpliterator split = new GeoParquetGroupSpliterator(
-            geoParquetReader, fileStatuses, mid, currentEndIndex);
+        geoParquetReader, fileStatuses, mid, currentEndIndex);
     this.currentEndIndex = mid;
     return split;
   }
