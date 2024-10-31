49781097d98870f1d76f0568f2737a162f7c7df5
Bertil Chapuis <bchapuis@gmail.com>
2024-10-31 14:43:23 +0100

    Add consumer, mapper and query for datatable abstration

---
+++ baremaps-core/src/main/java/org/apache/baremaps/geocoder/DataTableQuery.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.baremaps.geocoder;
+
+import org.apache.baremaps.geocoder.openstreetmap.OsmTags;
+import org.apache.lucene.queryparser.classic.QueryParserBase;
+import org.apache.lucene.queryparser.simple.SimpleQueryParser;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+
+public class DataTableQuery {
+
+  private final String query;
+
+  public DataTableQuery(String query) {
+    this.query = query;
+  }
+
+  public Query build() {
+    var builder = new BooleanQuery.Builder();
+    var queryTextEsc = QueryParserBase.escape(query);
+
+    var parser = new SimpleQueryParser(GeocoderConstants.ANALYZER, OsmTags.NAME.key());
+    var termsQuery = parser.parse(queryTextEsc);
+    // at least one terms of the queryText must be present
+    builder.add(termsQuery, BooleanClause.Occur.MUST);
+    return builder.build();
+  }
+}
