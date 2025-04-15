8db957038c5c2fb22f1231b41f67989e0943b9ca
Bertil Chapuis <bchapuis@gmail.com>
2025-04-15 15:11:00 +0200

    Add calcite schema for postgres

--- baremaps-calcite/src/main/java/org/apache/baremaps/calcite/postgres/PostgresSchema.java
+++ baremaps-calcite/src/main/java/org/apache/baremaps/calcite/postgres/PostgresSchema.java
@@ -1,43 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.baremaps.calcite.postgres;
 
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.sql.DataSource;
 import org.apache.baremaps.postgres.metadata.DatabaseMetadata;
 import org.apache.baremaps.postgres.metadata.TableMetadata;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
 import org.apache.calcite.schema.Table;
 import org.apache.calcite.schema.impl.AbstractSchema;
 
-import javax.sql.DataSource;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class PostgresSchema extends AbstractSchema {
 
-    private final DataSource dataSource;
-    private final String schemaName;
-    private final RelDataTypeFactory typeFactory;
+  private final DataSource dataSource;
+  private final String schemaName;
+  private final RelDataTypeFactory typeFactory;
 
-    public PostgresSchema(DataSource dataSource, String schemaName, RelDataTypeFactory typeFactory) {
-        this.dataSource = dataSource;
-        this.schemaName = schemaName;
-        this.typeFactory = typeFactory;
-    }
+  public PostgresSchema(DataSource dataSource, String schemaName, RelDataTypeFactory typeFactory) {
+    this.dataSource = dataSource;
+    this.schemaName = schemaName;
+    this.typeFactory = typeFactory;
+  }
 
-    @Override
-    protected Map<String, Table> getTableMap() {
-        DatabaseMetadata databaseMetadata = new DatabaseMetadata(dataSource);
-        Map<String, Table> tableMap = new HashMap<>();
-        try {
-            List<TableMetadata> tables = databaseMetadata.getTableMetaData(null, schemaName, null, new String[]{"TABLE"});
-            for (TableMetadata table : tables) {
-                String tableName = table.table().tableName();
-                Table calciteTable = new PostgresModifiableTable(dataSource, tableName, typeFactory);
-                tableMap.put(tableName, calciteTable);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return tableMap;
+  @Override
+  protected Map<String, Table> getTableMap() {
+    DatabaseMetadata databaseMetadata = new DatabaseMetadata(dataSource);
+    Map<String, Table> tableMap = new HashMap<>();
+    try {
+      List<TableMetadata> tables =
+          databaseMetadata.getTableMetaData(null, schemaName, null, new String[] {"TABLE"});
+      for (TableMetadata table : tables) {
+        String tableName = table.table().tableName();
+        Table calciteTable = new PostgresModifiableTable(dataSource, tableName, typeFactory);
+        tableMap.put(tableName, calciteTable);
+      }
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
+    return tableMap;
+  }
 }
--- baremaps-calcite/src/test/java/org/apache/baremaps/calcite/postgres/PostgresSchemaTest.java
+++ baremaps-calcite/src/test/java/org/apache/baremaps/calcite/postgres/PostgresSchemaTest.java
@@ -34,8 +34,8 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 /**
- * Tests for the PostgresSchema class, which provides access to PostgreSQL tables through the Calcite
- * framework.
+ * Tests for the PostgresSchema class, which provides access to PostgreSQL tables through the
+ * Calcite framework.
  */
 class PostgresSchemaTest extends PostgresContainerTest {
 
@@ -54,14 +54,14 @@ class PostgresSchemaTest extends PostgresContainerTest {
         Statement statement = connection.createStatement()) {
       // Ensure PostGIS extension is available
       statement.execute("CREATE EXTENSION IF NOT EXISTS postgis");
-      
+
       statement.execute("DROP TABLE IF EXISTS " + TEST_TABLE);
       statement.execute("CREATE TABLE " + TEST_TABLE + " (" +
           "id INTEGER PRIMARY KEY, " +
           "name VARCHAR(100), " +
           "description TEXT" +
           ")");
-      
+
       // Insert some test data
       statement.execute("INSERT INTO " + TEST_TABLE + " VALUES (1, 'Test 1', 'Description 1')");
       statement.execute("INSERT INTO " + TEST_TABLE + " VALUES (2, 'Test 2', 'Description 2')");
@@ -79,36 +79,36 @@ class PostgresSchemaTest extends PostgresContainerTest {
 
     // Verify that our test table is in the map
     assertTrue(tableMap.containsKey(TEST_TABLE), "Table map should contain test table");
-    
+
     // Verify that we can query the table through Calcite
     Properties info = new Properties();
     info.setProperty("lex", "MYSQL");
     info.setProperty("caseSensitive", "false");
-    
+
     try (Connection connection = DriverManager.getConnection("jdbc:calcite:", info)) {
       CalciteConnection calciteConnection = connection.unwrap(CalciteConnection.class);
       SchemaPlus rootSchema = calciteConnection.getRootSchema();
-      
+
       // Add our schema to the root schema
       rootSchema.add(TEST_SCHEMA, schema);
-      
+
       // Query the table
       try (Statement statement = connection.createStatement();
           ResultSet resultSet = statement.executeQuery(
               "SELECT * FROM " + TEST_SCHEMA + "." + TEST_TABLE + " ORDER BY id")) {
-        
+
         // Verify first row
         assertTrue(resultSet.next());
         assertEquals(1, resultSet.getInt("id"));
         assertEquals("Test 1", resultSet.getString("name"));
         assertEquals("Description 1", resultSet.getString("description"));
-        
+
         // Verify second row
         assertTrue(resultSet.next());
         assertEquals(2, resultSet.getInt("id"));
         assertEquals("Test 2", resultSet.getString("name"));
         assertEquals("Description 2", resultSet.getString("description"));
-        
+
         // Verify no more rows
         assertFalse(resultSet.next());
       }
